0741832c9d1939b04a2cd52ff72fd218f88331fc
oxod <git@oxod.nl>
2026-07-19 11:28:10 +0200

    feat: update project name and artifactId from Meteor Client to NekoClient

--- build.gradle.kts
+++ build.gradle.kts
@@ -218,7 +218,7 @@ publishing {
   publications {
     create<MavenPublication>("mavenJava") {
       from(components["java"])
-      artifactId = "meteor-client"
+      artifactId = "nekoclient"
 
       version = "${libs.versions.minecraft.get()}-SNAPSHOT"
     }
