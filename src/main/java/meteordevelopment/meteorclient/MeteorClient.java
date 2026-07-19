f9d2fa5c4c4cb72fc8691b6df5a116897ce7c18b
oxod <git@oxod.nl>
2026-07-19 11:31:18 +0200

    feat: update MOD_ID from meteor-client to nekoclient

--- src/main/java/meteordevelopment/meteorclient/MeteorClient.java
+++ src/main/java/meteordevelopment/meteorclient/MeteorClient.java
@@ -48,7 +48,7 @@ import java.io.File;
 import java.lang.invoke.MethodHandles;
 
 public class MeteorClient implements ClientModInitializer {
-    public static final String MOD_ID = "meteor-client";
+    public static final String MOD_ID = "nekoclient";
     public static final ModMetadata MOD_META;
     public static final String NAME;
     public static final Version VERSION;
